6cbd041f345b03018320a273ebd427be40f6b9de
ian douglas <ian.douglas@iandouglas.com>
2025-09-14 03:47:40 -0600

    wip

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,17 +10,27 @@ async function readJsonFile<T>(filename: string): Promise<T[]> {
 }
 
 export async function getAllPortfolioItems(): Promise<PortfolioItem[]> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const conferences = await readJsonFile<any>('conferences.json');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const talks = await readJsonFile<any>('talks.json');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const workshops = await readJsonFile<any>('workshops.json');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const blogs = await readJsonFile<any>('blog.json');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const videos = await readJsonFile<any>('videos.json');
 
   const allItems: PortfolioItem[] = [
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...conferences.map((item: any) => ({ ...item, category: 'conference' as const })),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...talks.map((item: any) => ({ ...item, category: 'talk' as const })),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...workshops.map((item: any) => ({ ...item, category: 'workshop' as const })),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...blogs.map((item: any) => ({ ...item, category: 'blog' as const })),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...videos.map((item: any) => ({ ...item, category: 'video' as const })),
   ];
 
